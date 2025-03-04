95d3755662485e1b2ef034c4105b04cf7f1dad86
ndoya28 <n.n16@zig.univ.sn>
2025-03-04 16:20:12 +0000

    Résolution du conflit sur persistence.xml

--- src/main/java/m1/uasz/sn/dao/EtudiantDAO.java
+++ src/main/java/m1/uasz/sn/dao/EtudiantDAO.java
@@ -1,25 +1,49 @@
 package m1.uasz.sn.dao;
 
+import java.util.List;
+
 import m1.uasz.sn.models.Etudiant;
+import m1.uasz.sn.models.Formation;
 
 public class EtudiantDAO extends GenericDAO<Etudiant, String> {
     public EtudiantDAO() {
         super(Etudiant.class);
     }
 
-    public void inscrireEtudiantAFormation(Long etudiantId, Long formationId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'inscrireEtudiantAFormation'");
+   public void inscrireEtudiantAFormation(Long etudiantId, Long formationId) {
+        em.getTransaction().begin();
+        Etudiant etudiant = findById(etudiantId);
+        Formation formation = em.find(Formation.class, formationId);
+        if (etudiant != null && formation != null) {
+            etudiant.setFormation(formation);
+            em.merge(etudiant);
+        }
+        em.getTransaction().commit();
     }
 
-    public void desincrireEtudiantAFormation(long etudiantId, long formationId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'desincrireEtudiantAFormation'");
-    }
 
-    public void inscrireEtudiantAModole(Long etudiantId, Long formationId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'inscrireEtudiantAModole'");
+    public void desinscrireEtudiantAFormation(long etudiantId, long formationId) {
+        em.getTransaction().begin();
+        
+        // Suppression de la relation dans la table de jointure
+        em.createQuery("DELETE FROM Etudiant_Formation ef WHERE ef.etudiant.id = :etudiantId AND ef.formation.id = :formationId")
+          .setParameter("etudiantId", etudiantId)
+          .setParameter("formationId", formationId)
+          .executeUpdate();
+        
+        em.getTransaction().commit();
+    }
+    
+    
+    public void inscrireEtudiantAModulr(Long etudiantId, Long moduleId) {
+        em.getTransaction().begin();
+        Etudiant etudiant = findById(etudiantId);
+        Module module = em.find(Module.class, moduleId);
+        if (etudiant != null && module != null) {
+            etudiant.setModule(module);
+            em.merge(etudiant);
+        }
+        em.getTransaction().commit();
     }
 
     public void desincrireEtudiantAModule(long etudiantId, long formationId) {
--- src/main/java/m1/uasz/sn/dao/NoteDAO.java
+++ src/main/java/m1/uasz/sn/dao/NoteDAO.java
@@ -1,7 +1,12 @@
 package m1.uasz.sn.dao;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import jakarta.persistence.Query;
+import m1.uasz.sn.models.Etudiant;
+import m1.uasz.sn.models.Module;
 import m1.uasz.sn.models.Note;
 
 public class NoteDAO extends GenericDAO<Note, Long> {
@@ -17,9 +22,20 @@ public class NoteDAO extends GenericDAO<Note, Long> {
         throw new UnsupportedOperationException("Unimplemented method 'calculerMoyenne'");
     }
 
-    public void saisirNote(Long etudiantId, Long moduleId, double noteCC, double noteExam) {
+   public void saisirNote(Long etudiantId, Long moduleId, double cc, double examen) {
+        Etudiant etudiant = new EtudiantDAO().findById(etudiantId);
+        Module module = new ModuleDAO().findById(moduleId);
         
+        Note note = new Note( cc,examen,etudiant,module);
+        // note.setEtudiant(etudiant);
+        // note.setModule(module);
+        // note.setNoteCC(cc);
+        // note.setNoteExamen(examen);
+        // note.calculerMoyenne();
+        
+        // NoteDAO.addNote(note);
     }
+
 
     public void deliberer() {
        
@@ -30,9 +46,21 @@ public class NoteDAO extends GenericDAO<Note, Long> {
         throw new UnsupportedOperationException("Unimplemented method 'calculerTauxReussite'");
     }
 
-    public Map<String, Integer> statistiquesMentions() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'statistiquesMentions'");
+  
+
+    public Map<String, Long> statistiquesMentions() {
+        String jpql = "SELECT n.mention, COUNT(n) FROM Note n GROUP BY n.mention";
+        Query query = em.createQuery(jpql);
+        List<Object[]> results = query.getResultList();
+
+        Map<String, Long> stats = new HashMap<>();
+        for (Object[] result : results) {
+            stats.put((String) result[0], (Long) result[1]);
+        }
+
+        return stats;
     }
-   
 }
+
+   
+
